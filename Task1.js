508ec9b99288fd2334479d4c4df5f3ae4a8f5e62
fajryalvin12 <165570283+fajryalvin12@users.noreply.github.com>
2024-06-03 18:14:30 +0700

    Add files via upload

--- Task1.js
+++ Task1.js
@@ -1,36 +1,33 @@
-const title = "Biodata Diri :"
-
-const fullName = "Fajry Alvin Hidayat"
-
-const age = 24
-
-const hobbies = ["Music", "Gaming", "Football"]
-
-const isMarried = true
-
-const schoolList = {
-    nameCollege: "Universitas Muhammadiyah Yogyakarta",
-    yearIn: 2018,
-    yearOut: 2022,
-    major: "International Relations"
-}
-
-const skills = {
-    skillName: "Javascript, HTML, CSS",
-    level: "Beginner"
-}
-
-const interestInCoding = true
-
-console.log(title)
-console.log("Nama Lengkap: " + fullName)
-console.log("Umur: " + age)
-console.log("Hobi: " + hobbies[1])
-console.log("Status Perkawinan: " + isMarried)
-console.log("Asal Sekolah/Kampus: " + schoolList.nameCollege)
-console.log("Tahun Masuk: " + schoolList.yearIn)
-console.log("Tahun Keluar: " + schoolList.yearOut)
-console.log("Jurusan: " + schoolList.major)
-console.log("Skill: " + skills.skillName)
-console.log("Level: " + skills.level)
-console.log("Tertarik dengan coding: " + interestInCoding)
+const biodata = {
+    name: "Fajry Alvin Hidayat",
+    age: 24,
+    hobbies: ["Gaming", "Football", "Listening a Music"],
+    isMarried: false,
+    schoolList: [ {
+        name: "Universitas Muhammadiyah Yogyakarta",
+        yearIn: 2018,
+        yearOut: 2022,
+        major: "International Relations"    
+    },
+    {
+        name: "Universitas Gadjah Mada",
+        yearIn: 2017,
+        yearOut: 2021,
+        major: "International Relations"
+    }
+],
+    skills: [ {
+        skillName: "JavaScript",
+        level: "Beginner"
+    },
+    {
+        skillName: "CSS",
+        level: "Beginner",
+    },
+    {
+        skillName: "HTML",
+        level: "Beginner",
+    },
+],
+    interestInCoding: true}
+    console.log(biodata.schoolList[1].yearIn)
